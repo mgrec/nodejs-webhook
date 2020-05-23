6c1bdb9cbfc1970aceceb5c4f9b6417afefe5dc1
subskill-mgrec <m.grec@subskill.com>
2020-05-24 01:00:50 +0200

    get body req #6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 var server = http.createServer(function(req, res) {
     if (req.method == 'POST') {
@@ -18,3 +18,22 @@ var server = http.createServer(function(req, res) {
     res.end('Salut tout le monde !');
 });
 server.listen(8080);
+*/
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.post('/', function (req, res) {
+    console.log('new push');
+    console.log(req.body);
+    //res.send('Hello World!')
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+});
