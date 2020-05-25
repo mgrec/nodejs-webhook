e16eb0cb7d13bd25e6aaa299271b933f9748145a
subskill-mgrec <m.grec@subskill.com>
2020-05-25 14:06:54 +0200

    test upload ci #6

--- index.js
+++ index.js
@@ -22,30 +22,29 @@ app.post('/', function (req, res) {
     let time   = Math.floor(Date.now() / 1000);
     let repo   = req.body.repository.html_url;
     let branch = gitCheck(req.body.ref);
-    fs.readFile('config/nodejs-webhook.json', (err, data) => {
-        if (err) throw err;
-        let config = JSON.parse(data);
-        console.log(config.config.host);
-    });
-
     if (branch){
         exec('mkdir git_temp_' + time);
         exec('cd git_temp' + time);
         exec('git clone -b dev '+ repo.toString() +' git_temp_' + time, (error, stdout, stderr) => {
-            uploader({
-                server: {
-                    host: '54.36.190.245',
-                    port: '22',
-                    username: 'root',
-                    password: 'E2l7ZczX'
-                },
-                locationBase: path.join(__dirname, 'git_temp_' + time),
-                serverBase: '/var/www/html/nodejs-wh-site/'
-            }).then(allDone => {
-                // Resolved When All File Uploaded
-                console.log('Deploy : OK!');
-                exec('cd ..');
-                exec('rm -rf git_temp' + time);
+            fs.readFile('config/nodejs-webhook.json', (err, data) => {
+                if (err) throw err;
+                let json = JSON.parse(data);
+                let config = json.config;
+                uploader({
+                    server: {
+                        host: config.host,
+                        port: config.port,
+                        username: config.user,
+                        password: config.password
+                    },
+                    locationBase: path.join(__dirname, 'git_temp_' + time),
+                    serverBase: config.path_upload
+                }).then(allDone => {
+                    // Resolved When All File Uploaded
+                    console.log('Deploy : OK!');
+                    exec('cd ..');
+                    exec('rm -rf git_temp' + time);
+                });
             });
         });
     }
