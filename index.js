052f5ec15734c6a813038d6748ff804d2fdf1f17
subskill-mgrec <m.grec@subskill.com>
2020-05-25 11:48:12 +0200

    test iim #5

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+function gitCheck(branch){
+    if(branch == "refs/heads/dev") {
+        return true;
+    }
+    return false;
+}
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
 app.post('/', function (req, res) {
     console.log('new push');
-    console.log(req.body.ref);
-    //console.log(req.body);
+    let branch = gitCheck(req.body.ref);
+    console.log(branch)
 });
 
 app.listen(8080, function () {
