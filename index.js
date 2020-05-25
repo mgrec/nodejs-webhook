c5d8634a2f2797de121dad55fc5daae898aa01c0
subskill-mgrec <m.grec@subskill.com>
2020-05-25 11:34:50 +0200

    test iim #2

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/*var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    if (req.method == 'POST') {
-        console.log("POST");
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            console.log("Partial body: " + body);
-        });
-        req.on('end', function () {
-            console.log("Body: " + body);
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
-    }
-    res.writeHead(200);
-    res.end('Salut tout le monde !');
-});
-server.listen(8080);
-*/
-
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -31,9 +9,8 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     console.log('new push');
     console.log(req.body);
-    //res.send('Hello World!')
 });
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Waiting push... listening on port 8080!');
 });
